8a30a849e7b48e739a660b207d13cdb6a63364e2
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-07-29 02:34:09 +0200

    write env via null r essource

--- modules/wordpress/main.tf
+++ modules/wordpress/main.tf
@@ -258,7 +258,7 @@ resource "docker_container" "workspace" {
   provisioner "local-exec" {
     command = <<EOT
       whoami
-      docker exec ${self.name} sh -c 'echo "${var.env}" > /var/www/html/.env'
+      sudo docker exec ${self.name} sh -c 'echo "${var.env}" > /var/www/html/.env'
     EOT
   }
 }
