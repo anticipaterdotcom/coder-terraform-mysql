cf0b4d6f472f874da06958896af50a2ee6ede5c8
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-08-01 22:08:05 +0200

    checkout single branch with missing repo

--- modules/wordpress/main.tf
+++ modules/wordpress/main.tf
@@ -130,7 +130,7 @@ resource "coder_agent" "wordpress" {
 
     cd /var/www/
     mkdir -p temp_dir
-    git clone --single-branch --branch ${var.branch} temp_dir
+    git clone --single-branch --branch ${var.branch} ${var.repo} temp_dir
     mv temp_dir/* /var/www/html
     rm -rf temp_dir --no-preserve-root
     cd /var/www/html
