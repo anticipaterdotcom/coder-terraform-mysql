f828e8bc4cb90e91f7ef8fef77745fa4abb2c461
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-08-06 01:11:18 +0200

    build external repo

--- modules/shopware/main.tf
+++ modules/shopware/main.tf
@@ -100,6 +100,8 @@ resource "coder_agent" "shopware" {
     cp /var/www/.env /var/www/html/.env
     mkdir -p /var/www/html/custom/plugins
     mkdir -p /var/www/html/custom/static-plugins
+
+    cd /var/www/html
     composer install --no-interaction --optimize-autoloader --no-suggest --no-scripts
 
     /entrypoint.sh >/tmp/dockware.log 2>&1 &
