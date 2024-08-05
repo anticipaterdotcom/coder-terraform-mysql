5ff50519c6fb9a47b1d99df5f1eb01ef943ca726
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-08-05 23:37:27 +0200

    debug startup

--- modules/shopware/main.tf
+++ modules/shopware/main.tf
@@ -141,7 +141,7 @@ resource "docker_image" "shopware" {
 resource "docker_container" "workspace" {
   count = data.coder_workspace.me.start_count
   image = docker_image.shopware.name
-  user = root
+  user = "root"
   # Uses lower() to avoid Docker restriction on container names.
   name = "coder-${lower(data.coder_workspace_owner.me.name)}-${lower(data.coder_workspace.me.name)}"
   # Hostname makes the shell more user friendly: coder@my-workspace:~$
