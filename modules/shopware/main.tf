6342f7c14b26928f181af8a38498de0cbbdc350d
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-08-06 01:08:52 +0200

    build external repo

--- modules/shopware/main.tf
+++ modules/shopware/main.tf
@@ -96,6 +96,7 @@ resource "coder_agent" "shopware" {
     git clone --single-branch --branch ${var.branch} ${var.repo} temp_dir
     mv temp_dir/* /var/www/html
     rm -rf temp_dir --no-preserve-root
+    chown www-data -Rf /var/www/html
     cp /var/www/.env /var/www/html/.env
     mkdir -p /var/www/html/custom/plugins
     mkdir -p /var/www/html/custom/static-plugins
