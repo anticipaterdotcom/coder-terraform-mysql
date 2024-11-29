655c7365268a77c997e5b155ffd4eb81a96b1572
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-11-29 18:41:18 +0100

    [PROV-148] prevent issue with variable

--- modules/shopware/main.tf
+++ modules/shopware/main.tf
@@ -79,7 +79,7 @@ variable "is_local" {
 
 resource "local_file" "htaccess" {
   content  = file("${path.module}/apache2/htaccess")
-  filename = "/var/www/.htaccess_placebear"
+  filename = "/opt/.htaccess_placebear"
 }
 
 data "coder_provisioner" "me" {}
@@ -165,7 +165,7 @@ resource "coder_agent" "shopware" {
     cd /var/www/html
 
     if [ "${var.placebear}" == "true" ]; then
-          cp /var/www/.htaccess_placebear .htaccess
+          cp /opt/.htaccess_placebear .htaccess
     fi
 
     rm -rf config/jwt/*
