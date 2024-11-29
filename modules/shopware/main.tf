2250048ec3953d810dada4ce28dc9d12b97ccf12
Thorsten Reiser <thorsten.reiser@dinited.com>
2024-11-29 18:21:58 +0100

    [PROV-148] prevent issue with variable

--- modules/shopware/main.tf
+++ modules/shopware/main.tf
@@ -160,18 +160,18 @@ resource "coder_agent" "shopware" {
     cd /var/www/html
 
     if [ "${var.placebear}" == "true" ]; then
-      touch .htaccess
-      echo '<IfModule mod_rewrite.c>
-                RewriteEngine On
+          touch .htaccess
+          echo '<IfModule mod_rewrite.c>
+                    RewriteEngine On
 
-                # Redirect thumbnail images
-                RewriteCond %{REQUEST_URI} ^/thumbnail/.+_(\d+)x(\d+)\.jpg$
-                RewriteRule ^ https://placebear.com/%1/%2 [R=301,L]
+                    # Redirect thumbnail images
+                    RewriteCond \%\{REQUEST_URI\} ^/thumbnail/.+_(\d+)x(\d+)\.(jpg|jpeg)\$
+                    RewriteRule ^ https://placebear.com/%1/%2 [R=301,L]
 
-                # Redirect media files
-                RewriteCond %{REQUEST_URI} ^/media/.+\.(svg|jpg|jpeg|png)$
-                RewriteRule ^ https://placebear.com/400/400 [R=301,L]
-        </IfModule>' > .htaccess
+                    # Redirect media files
+                    RewriteCond \%\{REQUEST_URI\} ^/media/.+\.(svg|jpg|jpeg|png)\$
+                    RewriteRule ^ https://placebear.com/400/400 [R=301,L]
+            </IfModule>' > .htaccess
     fi
 
     rm -rf config/jwt/*
